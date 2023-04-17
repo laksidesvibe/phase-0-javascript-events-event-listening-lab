286b41dffa252e34c49720e956fec9b17dcfd18d
lekan <laksidesvibe@gmail.com>
2023-04-16 22:27:37 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const button = document.getElementById('button');
+
+
 function addingEventListener() {
+
+
+
+alert('I was clicked!');
+button.addEventListener('click', addingEventListener);
 }
+addingEventListener()
