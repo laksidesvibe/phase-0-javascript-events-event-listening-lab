2f5dc0c88754769bc7eaf0f73f3bf1697ae402d2
lekan <laksidesvibe@gmail.com>
2023-05-01 02:04:03 -0400

    redone

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const button = document.getElementById('button');
 
 
 function addingEventListener() {
-
-
-
-alert('I was clicked!');
-button.addEventListener('click', addingEventListener);
-}
-addingEventListener()
+    alert('I was clicked!');
+    button.addEventListener('click', addingEventListener);
+};
